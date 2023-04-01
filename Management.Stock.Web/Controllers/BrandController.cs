d2f89460d6415a391792c08a63f07d53a933738a
soraxg12 <contadevervidio@gmail.com>
2023-04-01 16:27:14 -0300

    add controllers

---
+++ Management.Stock.Web/Controllers/BrandController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Management.Stock.Web.Controllers
+{
+    [Route("[controller]")]
+    public class BrandController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("create")]
+        public IActionResult create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Management.Stock.Web/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Management.Stock.Web.Controllers
+{
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("create")]
+        public IActionResult create()
+        {
+            return View();
+        }
+    }
+}
